b1c3f8aed6e26ae6d4bc5681df3a12f02852397a
regchiu <regx1385@gmail.com>
2023-07-12 09:59:10 +0800

    chore: vite.config.ts base

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,37 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import checker from 'vite-plugin-checker'
 import { fileURLToPath, URL } from 'url'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    checker({
-      vueTsc: true,
-      stylelint: {
-        lintCommand: 'stylelint ./src/**/*.{scss,vue}'
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    plugins: [
+      vue(),
+      checker({
+        vueTsc: true,
+        stylelint: {
+          lintCommand: 'stylelint ./src/**/*.{scss,vue}'
+        }
+      })
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
       }
-    })
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  build: {
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true
+    },
+    build: {
+      minify: 'terser',
+      terserOptions: {
+        compress: {
+          drop_console: true
+        }
       }
-    }
-  },
-  server: {
-    host: '0.0.0.0'
+    },
+    server: {
+      host: '0.0.0.0'
+    },
+    base: env.NODE_ENV === 'production' ? '/diablo4-map/' : '/'
   }
 })
