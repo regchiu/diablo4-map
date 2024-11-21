f6270a12d9b1f2324467e819af1bdfefde3ca29f
regchiu <regx1385@gmail.com>
2024-11-21 15:23:28 +0800

    chore: update deps version

--- eslint.config.js
+++ eslint.config.js
@@ -7,7 +7,7 @@ import eslintConfigPrettier from 'eslint-config-prettier'
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   { files: ['**/*.{js,mjs,cjs,ts,vue}'] },
-  { languageOptions: { globals: globals.browser } },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   ...pluginVue.configs['flat/recommended'],
